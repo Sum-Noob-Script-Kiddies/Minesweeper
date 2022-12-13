ff10594da860e99bb37a35596db2f944269a9736
SeanTheBeanstalk <119796169+SeanTheBeanstalk@users.noreply.github.com>
2022-12-13 22:54:45 +0800

    timer updated

--- main.py
+++ main.py
@@ -11,10 +11,16 @@ screen_rect = screen.get_rect()
 cellstyle = UI.CellButtonStyle(("assets/cell/idle.png", "assets/cell/hover.png"), "assets/cell/flag.png", "assets/cell/mine.png", "assets/cell/")
 minefield = minesweeper.Minefield(screen_rect.center, cellstyle, mode=GAMEMODE)     # Creates a minefield with the given cellstyle and mode
 
-font = pg.font.Font('Rare Game.otf', 32)
-green = (0, 255, 0)
+switch = False
+
+font = pg.font.Font("assets/fonts/Rare Game.otf", 32)
+green = COLOR_LIGHT
 cur_dur = 0
 
+def start_timer():
+    start_ticks = pg.time.get_ticks()
+    return start_ticks
+    
 while True: 
     for event in pg.event.get():    # Event Loop
         if event.type == pg.QUIT:
@@ -28,16 +34,24 @@ while True:
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_SPACE:
                 minefield.toggle_pause()
+        if event.type == GAMESTART:
+            switch = True
+            start_ticks = start_timer()
+
+    if switch:
+        if cur_dur < 1000:
+            cur_dur = (pg.time.get_ticks() - start_ticks)//1000
+        else:
+            cur_dur = 0
 
     screen.fill(COLOR_DARK)   # Render the screen's background
     minefield.draw_board()  # Render the cells onto the board
     screen.blit(minefield.board, minefield.board_rect)  # Render the board onto the screen
-    
-    if cur_dur < 1000:
-        cur_dur = pg.time.get_ticks()//1000
+
     text = font.render(str(cur_dur), True, green)
     textRect = text.get_rect()
     textRect.center = (100, 100)
     screen.blit(text, textRect)
 
     pg.display.update()
+
